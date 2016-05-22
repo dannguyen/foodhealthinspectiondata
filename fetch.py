a63bc4d890b7e5289739de63f4b9aba4821ab61a
Dan Nguyen <dansonguyen@gmail.com>
2016-05-22 01:12:39 -0700

    orange louisville

--- fetch.py
+++ fetch.py
@@ -32,21 +32,21 @@ for d in csv.DictReader(requests.get(LIST_URL).text.splitlines()):
         print(xpath, "already exists")
         sdir = xdir.joinpath('samples')
         sdir.mkdir(parents=True, exist_ok=True)
-        if 'csv' in xpath.name:
+        if 'csv' in xpath.name or 'txt' in xpath.name:
             sdir.joinpath(bname).write_text("\n".join(xpath.read_text().splitlines()[0:1000]))
         else: # zipfile
             zfile = ZipFile(xpath.open('rb'))
             for n in zfile.namelist():
-                zfile.extract(n, path=str(xdir))
+                zfile.extract(n, path=str(xdir.joinpath('raw')))
                 ypath = sdir.joinpath(n)
                 print(ypath)
-                with xdir.joinpath(n).open('r', encoding='latin1') as rf:
+                with xdir.joinpath('raw', n).open('r', encoding='latin1') as rf:
                     ypath.write_text('\n'.join(rf.readlines()[0:1000]))
 
     else:
         print("Downloading", url, "\n to", xpath)
         resp = requests.get(url)
-        if 'csv' in xpath.name:
+        if 'csv' in xpath.name or 'txt' in xpath.name:
             xpath.write_text(resp.text)
         else: # zip file
             xpath.write_bytes(resp.content)
